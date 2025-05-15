653be0b6cd21bebffffdff7564d4351c06467900
Tal Koren <ardethian@gmail.com>
2025-05-15 17:57:00 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ export const TRACKING_EVENTS = {
 const CHANNEL_11_URL = 'https://kan11.media.kan.org.il/hls/live/2024514/2024514/master.m3u8';
 const CHANNEL_12_URL = 'https://mako-streaming.akamaized.net/stream/hls/live/2033791/k12makowad/index.m3u8?b-in-range=0-1800&_uid=8b6d7d32-3434-42f8-a115-ce9b04631681&user_id=6cf04f139114b11f566be3fa96891043&dxu=a9d78826-705e-41da-9d38-7147b7db6a1b&hdnea=st%3D1740136674%7Eexp%3D1740137574%7Eacl%3D%2F*%7Ehmac%3D07ba33148e6006c62be39a7d00b7169e7b6c813baa0d1346699840472bbdf07b';
 const CHANNEL_12_TICKET_PREFETCH_URL = 'https://mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=egt';
-const CHANNEL_13_URL = 'https://reshet.g-mana.live/media/87f59c77-03f6-4bad-a648-897e095e7360/profile/0/profileManifest.m3u8?_uid=1e106bfe-96db-407d-848f-7003ffffb6a1&rK=a6&_did=732c2dc6-38f5-4436-ac26-fb2b41ec44ca';
+const CHANNEL_13_URL = 'https://d2lckchr9cxrss.cloudfront.net/out/v1/c73af7694cce4767888c08a7534b503c/index.m3u8';
 const I24_NEWS_URL = 'https://bcovlive-a.akamaihd.net/d89ede8094c741b7924120b27764153c/eu-central-1/5377161796001/playlist.m3u8';
 const SEINFELD_URL = 'https://demo.streamplanet.tv/hls/seinfeld/src/index.m3u8';
 
