81479e29bed1dfc7cd511e475ba7a01867ce72ac
Ethan Koren <ardethian@gmail.com>
2026-07-12 00:04:25 +0300

    refactor: convert FadingBox to CSS Modules

--- src/components/FadingBox/FadingBox.jsx
+++ src/components/FadingBox/FadingBox.jsx
@@ -1,32 +1,8 @@
-import styled from '@emotion/styled';
 import React from 'react';
-
-const ANIMATION_DURATION_MILLISECONDS = 450;
+import styles from './FadingBox.module.css';
 
 export const FadingBox = ({ shouldShow, children }) => (
-  <Box className={`${shouldShow ? 'fade-in' : 'fade-out'}`}>{children}</Box>
+  <div className={`${styles.box} ${shouldShow ? styles.visible : styles.hidden}`}>
+    {children}
+  </div>
 );
-
-const Box = styled.div`
-  &.fade-in {
-    pointer-events: all;
-    transform: scale(1);
-    opacity: 1;
-  }
-
-  &.fade-out {
-    pointer-events: none;
-    transform: scale(0.7);
-    opacity: 0;
-  }
-
-  transition: all ${ANIMATION_DURATION_MILLISECONDS}ms ease-out;
-  background: #000;
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  display: flex;
-  align-content: center;
-  justify-content: center;
-`;
